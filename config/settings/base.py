660141fc92e3340a4453728ca213e8c696c850ff
박건희 <baggeonhui@baggeonhuiui-Macmini.local>
2021-12-04 17:42:22 +0900

    로그인 기능 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -78,8 +78,10 @@ THIRD_PARTY_APPS = [
 ]
 
 LOCAL_APPS = [
-    "djangogram.users",
+    # "djangogram.users",
     # Your stuff: custom apps go here
+    "djangogram.users.apps.UsersConfig",
+    "djangogram.posts.apps.PostsConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
