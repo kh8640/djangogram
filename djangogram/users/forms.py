ee66c575b482b9c5fc0f292207c6ee266deb86c3
박건희 <baggeonhui@baggeonhuiui-Macmini.local>
2021-12-19 01:19:09 +0900

    회원가입 추가

--- djangogram/users/forms.py
+++ djangogram/users/forms.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import forms as admin_forms
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.forms import widgets
 from django.utils.translation import gettext_lazy as _
+from django import forms as django_forms
+
 
 User = get_user_model()
 
@@ -14,6 +18,41 @@ class UserCreationForm(admin_forms.UserCreationForm):
     class Meta(admin_forms.UserCreationForm.Meta):
         model = User
 
-        error_messages = {
-            "username": {"unique": _("This username has already been taken.")}
+    def clean_username(self):
+        username = self.cleaned_data["username"]
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            return username
+
+        raise ValidationError(self.error_messages["duplicate_username"] )
+
+        # error_messages = {
+        #     "username": {"unique": _("This username has already been taken.")}
+        # }
+
+
+class SignUpForm(django_forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email', 'name', 'username', 'password']
+
+        labels = {
+            'email' : '이메일 주소',
+            'name' : '성명',
+            'username' : '사용자 이름',
+            'password' :  '비밀번호'
         }
+
+        widgets = {
+            'password' : django_forms.PasswordInput(),
+        }
+
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
--- djangogram/users/views.py
+++ djangogram/users/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
+from .forms import SignUpForm
 
 @csrf_exempt
 def main(request):
@@ -19,3 +20,26 @@ def main(request):
             return HttpResponseRedirect(reverse('posts:index'))
         else:
             return render(request, 'users/main.html')
+
+def signup(request):
+    if request.method == 'GET' :
+        form = SignUpForm()
+
+        return render(request, 'users/signup.html', {'form' : form})
+
+    elif request.method == 'POST' :
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return HttpResponseRedirect(reverse('posts:index'))
+                
+        return render(request, 'users/main.html')
