c3b6b8564c83b339489ffbdfe1db8ac390c0a3af
박건희 <baggeonhui@baggeonhuiui-Macmini.local>
2021-12-23 15:12:07 +0900

    테스트코드오타  수정

--- djangogram/posts/views.py
+++ djangogram/posts/views.py
@@ -1,29 +1,29 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import render, get_object_or_404
 from djangogram.users.models import User as user_model
 
 from . import models
 
 def index(request):
-  return render(request, 'posts/base.html')
+    return render(request, 'posts/base.html')
 
 def post_create(request):
-  if request.method == 'GET':
-    return render(request, 'posts/post_create.html')
+    if request.method == 'GET':
+        return render(request, 'posts/post_create.html')
 
-  elif request.method == 'POST':
-    if request.user.is_authenticated:
-      user = get_object_or_404(user_model, pk=request.user.id)
-      image = request.FILES['image']
-      caption = request.POST['cation']
+    elif request.method == 'POST':
+        if request.user.is_authenticated:
+            user = get_object_or_404(user_model, pk=request.user.id)
+            image = request.FILES['image']
+            caption = request.POST['caption']
 
-      new_post = models.Post.objects.create(
-        author = user, 
-        image = image,
-        caption = caption
-      )
-      new_post.save()
+            new_post = models.Post.objects.create(
+                author = user,
+                image = image,
+                caption = caption
+            )
+            new_post.save()
 
-      return render(request, 'posts/base.html')
+            return render(request, 'posts/base.html')
 
-    else:
-      return render(request, 'users/main.html')
+        else:
+            return render(request, 'users/main.html')
